000fee608bd2b7243ea0e804c52e8ca7dde06928
Ubuntu_LAB <daniel90831581@gmail.com>
2024-08-18 20:27:55 +0800

    update by daniel

--- CRAZYFLIE/ONBOARD/aideck_gap8_projects/examples/other/lin_cluster_test_direct_sendimg/wifi-img-streamer.c
+++ CRAZYFLIE/ONBOARD/aideck_gap8_projects/examples/other/lin_cluster_test_direct_sendimg/wifi-img-streamer.c
@@ -404,7 +404,7 @@ int main(void) {
     pi_bsp_init();
 
     // Increase the FC freq to 250 MHz
-    pi_freq_set(PI_FREQ_DOMAIN_FC, 250000000);
+    //pi_freq_set(PI_FREQ_DOMAIN_FC, 250000000);
     __pi_pmu_voltage_set(PI_PMU_DOMAIN_FC, 1200);
 
     return pmsis_kickoff((void *)start_example);
